4beafb6f847a1b1d082c79c2c3513956df86c0a7
Tiago Gomes <tidgomes@gmail.com>
2023-11-09 09:06:22 -0300

    use prepared statement to insert data

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,8 @@
 package application;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.text.SimpleDateFormat;
 
 import db.DB;
 
@@ -11,23 +10,28 @@ public class Program {
 
 	public static void main(String[] args) {
 		Connection conn = null;
-		Statement st = null;
-		ResultSet rs = null;
+		PreparedStatement st = null;
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
 		try {
 			conn = DB.getConnection();
-			st = conn.createStatement();
 
-			rs = st.executeQuery("select * from department");
+			st = conn.prepareStatement("INSERT INTO seller " + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES " + "(?, ?, ?, ?, ?)");
 
-			while (rs.next()) {
-				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
-			}
-		} catch (SQLException e) {
+			st.setString(1, "Mano Pingado");
+			st.setString(2, "manoPin@mail.com");
+			st.setDate(3, new java.sql.Date(sdf.parse("18/06/1994").getTime()));
+			st.setDouble(4, 10000);
+			st.setInt(5, 1);
+
+			int rowsAffected = st.executeUpdate();
+			System.out.println("Done! Rows affected: " + rowsAffected);
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			DB.closeStateMent(st);
-			DB.closeResultSet(rs);
 			DB.closeConnection();
 		}
 	}
